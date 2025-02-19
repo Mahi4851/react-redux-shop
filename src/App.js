9d12bfc8fd18c9415d34a060026907cfb5c582bd
Mahi4851 <yaismahendra@gmail.com>
2025-02-20 01:05:27 +0530

    rechanges

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
           <Route path="/" element={<LandingPage />} />
           <Route path="/products" element={<ProductListing />} />
           <Route path="/cart" element={<ShoppingCart />} />
-          <Route path="/cart" element={<ShoppingCart />} />
+         
         </Routes>
       </Router>
     </Provider>
